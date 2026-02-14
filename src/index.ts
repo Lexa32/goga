1f6255ac90f5ce838316fa517fe248b1be0ee76f
Lexa32 <ayrung322@gmail.com>
2026-02-14 12:23:29 +0300

    denis

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Elysia } from "elysia";
-import z, { date } from "zod/v4";
+import z, { date, email } from "zod/v4";
 
 import { and, eq, gt, gte, lt, lte, not, or } from "drizzle-orm";
 import { db } from "./db";
@@ -31,100 +31,117 @@ const PostService = z.object({
 });
 
 type User = z.infer<typeof UserSchema> & { id: string };
-type Service = z.infer<typeof PostService> & { id: string } & { data: Date};
+type Service = z.infer<typeof PostService> & { id: string } & { data: Date };
 
 const userService = new Elysia({ prefix: "/users" })
     .get("/", async () => {
         return await db.query.users.findMany();
     })
-    // .get("/:id", ({ params, status }) => {
-    //     const user = users.find((user) => {
-    //         return user.id === params.id;
-    //     });
-    //     if (user === undefined) {
-    //         return status(404);
-    //     }
-    //     return user;
-    // })
+    .get("/:id", async ({ params, status }) => {
+        const oneuser = await db.query.users.findFirst({
+            where: eq(users.id, params.id)
+
+        })
+
+        if (oneuser === undefined) {
+            return status(404);
+        }
+        return oneuser;
+    })
     .post(
         "/",
         async ({ body }) => {
-            await db.insert(users).values({ 
-                ...body, 
-                id: Bun.randomUUIDv7() 
+            await db.insert(users).values({
+                ...body,
+                id: Bun.randomUUIDv7()
             });
         },
         {
             body: UserSchema,
         }
-    );
+    )
 
+
+    .put(
+        "/:id",
+        async ({ body, params }) => {
+            await db
+                .update(users)
+                .set(body)
+                .where(eq(users.id, params.id));
+        },
+        { body: UserSchema }
+    )
+
+    // Решил проверить обновление только 1 параметра, но по ощущениям сделал нерационально
+    .put(
+        "/email/:id",
+        async ({ body, params }) => {
+            await db
+                .update(users)
+                .set({
+                    ...body,
+                    email: body.email
+                }     
+                )
+                .where(eq(users.id, params.id));
+        },
+        { body: UserSchema }
+    )
+
+    .delete("/:id", async ({ params }) => {
+        await db.delete(users)
+            .where(eq(users.id, params.id))
+    });
+
+const postService = new Elysia({ prefix: "/service" })
+    .get("/", async () => {
+        return await db.query.posts.findMany();
+    })
+
+    .get("/:id", async({ params, status }) => {
+
+        const post = await db.query.posts.findFirst({
+            where: eq(posts.id, params.id)
+        })
+
+        if (post === undefined) {
+            return status(404);
+        }
+        return post;
+    })
+
+    // .post(
+    //     "/",
+    //     async ({ body }) => {
+    //     await db.insert(posts).values({
+    //         ...body,
+    //         id: Bun.randomUUIDv7(),
+    //         createdAt: new Date()
+    //     })
+    // },
+    // {
+    //     body: PostService
+    // })
+        
+    .put(
+        "/:id",
+        async ({ body, params }) => {
+            await db
+                .update(posts)
+                .set(body)
+                .where(eq(posts.id, params.id));
+        },
+        { body: PostService }
+    )
+
+    .delete("/:id", async ({ params }) => {
+        await db.delete(posts)
+        .where(eq(posts.id, params.id))
+    });
     
-    // .put(
-    //     "/:id",
-    //     ({ body, params }) => {
-    //         users = users.map((user) => {
-    //             if (user.id === params.id) {
-    //                 return {
-    //                     ...body,
-    //                     id: params.id,
-    //                 };
-    //             }
-    //             return user;
-    //         });
-    //     },
-    //     { body: UserSchema },
-    // )
-    // .delete("/:id", ({ params }) => {
-    //     users = users.filter((user) => {
-    //         return user.id !== params.id;
-    //     });
-    // });
 
-// const postService = new Elysia({ prefix: "/service" })
-//     .get("/", () => {
-//         return services;
-//     })
-//     .get("/:id", ({ params, status }) => {
-//         const service = services.find((service) => {
-//             return service.id === params.id;
-//         });
-//         if (service === undefined) {
-//             return status(404);
-//         }
-//     })
-//     .post(
-//         "/",
-//         ({ body }) => {
-//             services.push({ ...body, id: Bun.randomUUIDv7(), data: new Date()});
-//         },
-//         {
-//             body: PostService,
-//         },
-//     )
-//     .put(
-//         "/:id",
-//         ({ body, params }) => {
-//             services = services.map((service) => {
-//                 if (service.id === params.id) {
-//                     return {
-//                         ...service,
-//                         ...body,
-//                         id: params.id,
-//                     };
-//                 }
-//                 return service;
-//             });
-//         },
-//         { body: PostService },
-//     )
-//     .delete("/:id", ({ params }) => {
-//         services = services.filter((service) => {
-//             return service.id !== params.id;
-//         });
-//     });
 
-    
 
 new Elysia()
     .use(userService)
